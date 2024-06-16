a6e2ac583a005bd603d26cf7c554c49392865669
spurger <spurger93@gmail.com>
2024-06-16 22:00:39 +0200

    use bootstrap nav pills for navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,24 +1,28 @@
 <nav>
-    <ul>
-        <li>
-            <a href="{{ route('articles.index') }}">Articles</a>
+    <ul class="nav nav-pills">
+        <li class="nav-item">
+            <a class="nav-link @if (request()->routeIs('articles.index')) active @endif"
+                href="{{ route('articles.index') }}">Articles</a>
         </li>
-        <li>
-            <a href="{{ route('about') }}">About</a>
-        </li>
-        <li>
-            <a href="{{ route('admin') }}">Admin</a>
+        <li class="nav-item">
+            <a class="nav-link @if (request()->routeIs('about')) active @endif" href="{{ route('about') }}">About</a>
         </li>
         @auth
-            <li>
-                <a href="{{ route('articles.create') }}">Create article</a>
+            <li class="nav-item">
+                <a class="nav-link @if (request()->routeIs('articles.create')) active @endif"
+                    href="{{ route('articles.create') }}">Create article</a>
             </li>
-            <li>
+        @endauth
+        <li class="nav-item ms-auto">
+            <a class="nav-link @if (request()->routeIs('admin')) active @endif" href="{{ route('admin') }}">Admin</a>
+        </li>
+        @auth
+            <li class="nav-item">
                 <form method="POST" action="{{ route('logout') }}">
                     @method('DELETE')
                     @csrf
 
-                    <button type="submit">Logout</button>
+                    <button type="submit" class="nav-link">Logout</button>
                 </form>
             </li>
         @endauth
