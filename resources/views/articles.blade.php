e1e70b2a12de2f3697a0ccf044d892c837e5907b
spurger <spurger93@gmail.com>
2024-06-16 22:40:53 +0200

    change articles display  show empty state

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -3,13 +3,17 @@
 @section('title', 'Articles')
 
 @section('content')
-    <h1>Articles</h1>
-    @foreach ($articles as $article)
-        <article>
-            <a href="{{ route('articles.show', ['article' => $article]) }}">
-                <h2>{{ $article->title }}</h2>
-            </a>
-            <pre>{{ $article->full_text }}</pre>
+    <h1 class="mb-3">Articles</h1>
+    @forelse ($articles as $article)
+        <article class="mb-3">
+            <h2 class="fs-5">
+                <a href="{{ route('articles.show', ['article' => $article]) }}" class="mb-3">
+                    {{ $article->title }}
+                </a>
+            </h2>
+            <div class="text-truncate fs-6">{{ $article->full_text }}</div>
         </article>
-    @endforeach
+    @empty
+        <div class="alert alert-warning">No articles found.</div>
+    @endforelse
 @endsection
