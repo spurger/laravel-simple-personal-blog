df82f50a3a54a0f3f96af51785c1c5b8435c55aa
spurger <spurger93@gmail.com>
2024-06-16 21:15:21 +0200

    put article routes into auth middleware

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/articles');
 
 Route::view('/about', 'about')->name('about');
-Route::resource('articles', ArticleController::class)->only(['index', 'show', 'create', 'store']);
+Route::resource('articles', ArticleController::class)->only(['index', 'show']);
 
 Route::view('/login', 'auth.login')->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.store');
@@ -15,4 +15,6 @@ Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
     Route::view('/admin', 'admin')->name('admin');
+
+    Route::resource('articles', ArticleController::class)->only(['create', 'store']);
 });
