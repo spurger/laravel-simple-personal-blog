4a309d4e37f77324831bafeda522e3952fca22a3
spurger <spurger93@gmail.com>
2024-06-18 13:17:30 +0200

    add create article button to articles.index

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -7,6 +7,11 @@
         <div class="col-lg-6 offset-lg-3">
             <div class="mb-3">
                 <h1>Articles</h1>
+                @auth
+                    <div>
+                        <a href="{{ route('articles.create') }}" class="btn btn-primary">Create article</a>
+                    </div>
+                @endauth
             </div>
             @forelse ($articles as $article)
                 <x-article class="mb-3" :article="$article" />
